e598be435fbddde0c87711c7f80f9d0969d86f5c
hossein <Ali.dragon.swr@gmail.com>
2017-01-25 15:37:18 +0330

    adding http handling

--- server.js
+++ server.js
@@ -3,6 +3,13 @@ var app = express();
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
 const PORT = process.env.PORT || 3000;
+app.use(function(req,res,next){
+  if(req.headers['x-forwarded-proto'] === 'http'){
+    next();
+  } else {
+    res.redirect('http://' + req.hostname + req.url);
+  }
+});
 app.use(express.static('public'));
 
 // heres the server
