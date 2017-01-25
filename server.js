7d0c111a6878fb29e0c91be4d549c34d6029e2af
hossein <Ali.dragon.swr@gmail.com>
2017-01-25 15:18:44 +0330

    preparing

--- server.js
+++ server.js
@@ -2,17 +2,16 @@ var express = require('express');
 var app = express();
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({extended:true}));
-app.use(morgan('dev'));
+const PORT = process.env.PORT || 3000;
 app.use(express.static('public'));
 
 // heres the server
 
-app.listen(3000,function(err){
+app.listen(PORT,function(err){
   if(err){
   console.log(err);
 }
   else {
-    console.log('Server is Up on 3000');
+    console.log('Server is Up on '+PORT);
   }
 });
